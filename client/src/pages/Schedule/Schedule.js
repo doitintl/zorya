c73c02eeab2dab761515366a035e195454a2f8d9
drorlevy <drorlevy@gmail.com>
2018-02-15 19:31:53 +0200

    Schedule: toggle row/column

--- client/src/pages/Schedule/Schedule.js
+++ client/src/pages/Schedule/Schedule.js
@@ -5,28 +5,42 @@ import classNames from 'classnames';
 import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
 // import Divider from 'material-ui/Divider';
-import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
+import Button from 'material-ui/Button';
 
 // Lodash
 import map from 'lodash/map';
+import times from 'lodash/times';
+import includes from 'lodash/includes';
+import constant from 'lodash/constant';
 
 // Project
 import ScheduleService from '../../modules/api/schedule';
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const hours = [...Array(24).keys()];
 
 const styles = theme => ({
   root: {
     padding: theme.spacing.unit,
   },
+  row: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
   boxSize: {
     height: '36px',
     width: '36px',
+    margin: theme.spacing.unit / 4,
+  },
+  hourButtonRoot: {
+    minHeight: '36px',
+    minWidth: '36px',
   },
   longBoxSize: {
     height: '36px',
     width: '100px',
+    margin: theme.spacing.unit / 4,
   },
   boxContent: {
     display: 'flex',
@@ -34,10 +48,13 @@ const styles = theme => ({
     justifyContent: 'center'
   },
   on: {
-    backgroundColor: theme.palette.primary.main,
+    backgroundColor: theme.palette.primary.dark,
   },
   off: {
-    backgroundColor: theme.palette.secondary.light,
+    backgroundColor: theme.palette.secondary.dark,
+  },
+  clickable: {
+    cursor: 'pointer'
   }
 });
 
@@ -58,7 +75,7 @@ class Schedule extends React.Component {
     console.log(schedule.__ndarray__);
     this.setState({
       schedule,
-      matrix: schedule.__ndarray__
+      matrix: schedule.__ndarray__.slice()
     });
   }
 
@@ -66,45 +83,64 @@ class Schedule extends React.Component {
     const { classes } = this.props;
 
     const hourBlocks = map(dayValues, (value, index) => (
-      <Grid key={`day-${dayIndex}-hour-${index}`} item>
+      <div key={`day-${dayIndex}-hour-${index}`}>
         {
           value ?
             (
               <Paper className={classNames(classes.boxSize, classes.on)} elevation={2} />
             ) : (
-              <Paper className={classNames(classes.boxSize)} elevation={2} />
+              <Paper className={classNames(classes.boxSize, classes.off)} elevation={2} />
             )
         }
-      </Grid>
+      </div>
     ))
 
     return [
-      <Grid key={`day-${dayIndex}`} item>
-        <div className={classNames(classes.longBoxSize, classes.boxContent)}>
-          <Typography variant="caption" color="textSecondary">
+      <div key={`day-${dayIndex}`}>
+        <div className={classNames(classes.longBoxSize, classes.boxContent, classes.clickable)}>
+          {/* <Typography variant="caption" color="textSecondary" onClick={this.toggleDay(dayIndex)}>
             {days[dayIndex]}
-          </Typography>
+          </Typography> */}
+          <Button disableRipple disableFocusRipple variant="raised" className={classNames(classes.longBoxSize)} onClick={this.toggleDay(dayIndex)}>
+            {days[dayIndex]}
+          </Button>
         </div>
-      </Grid>,
+      </div>,
       ...hourBlocks
     ]
   }
 
+  toggleDay = dayIndex => event => {
+    this.setState((prevState, props) => {
+      const { matrix } = prevState;
+      if (includes(matrix[dayIndex], 0)) {
+        matrix[dayIndex] = times(24, constant(1));
+      } else {
+        matrix[dayIndex] = times(24, constant(0));
+      }
+      return { matrix };
+    })
+  }
+
+  toggleHour = hourIndex => event => {
+    this.setState((prevState, props) => {
+      const { schedule, matrix } = prevState;
+      const hourHasZero = !(matrix[0][hourIndex] && matrix[1][hourIndex] && matrix[2][hourIndex] && matrix[3][hourIndex] && matrix[4][hourIndex] && matrix[5][hourIndex] && matrix[6][hourIndex]);
+      const nextVal = hourHasZero ? 1 : 0;
+      for (let i = 0; i < schedule.Shape[0]; i++) {
+        matrix[i][hourIndex] = nextVal;
+      }
+      return { matrix };
+    })
+  }
+
   render() {
     const { classes } = this.props;
-    const { schedule } = this.state;
-
-    const hours = [...Array(25).keys()];
+    const { schedule, matrix } = this.state;
 
     return (
-
       schedule &&
       <div className={classes.root}>
-        {/* <Typography variant="subheading" color="textSecondary" gutterBottom>
-          {match.params.schedule}
-          <Divider />
-        </Typography> */}
-
         <Typography variant="body2" color="textSecondary">
           Name: {schedule.name}
         </Typography>
@@ -112,42 +148,34 @@ class Schedule extends React.Component {
           Timezone: {schedule.timezone}
         </Typography>
 
-
-        <Grid container justify="center" spacing={8}>
-
-          <Grid container item wrap="nowrap">
+        <div>
+          <div className={classes.row}>
+            <div className={classNames(classes.longBoxSize, classes.boxContent)}>
+              <Typography variant="button" color="textSecondary">
+                Day \ Hour
+              </Typography>
+            </div>
             {
               map(hours, hour =>
-                <Grid key={`hour-${hour}`} item>
-                  {hour > 0 ?
-                    (
-                      <div className={classNames(classes.boxSize, classes.boxContent)}>
-                        <Typography variant="caption" color="textSecondary">
-                          {
-                            hour <= 10 ? `0${hour - 1}` : hour - 1
-                          }
-                        </Typography>
-                      </div>
-                    ) : (
-                      <div className={classNames(classes.longBoxSize, classes.boxContent)}>
-                        <Typography variant="caption" color="textSecondary">
-                          Hours =>
-                        </Typography>
-                      </div>
-                    )
-                  }
-                </Grid>)
+                <div key={`hour-${hour}`}>
+                  <div className={classNames(classes.boxSize, classes.boxContent)}>
+                    <Button disableRipple disableFocusRipple variant="raised" classes={{ root: classes.hourButtonRoot }}  onClick={this.toggleHour(hour)}>
+                      {
+                        hour < 10 ? `0${hour}` : hour
+                      }
+                    </Button>
+                  </div>
+                </div>)
             }
-          </Grid>
-
+          </div>
 
           {
-            map(schedule.__ndarray__, (dayValues, dayIndex) =>
-              <Grid key={`${dayIndex}`} container item>
+            map(matrix, (dayValues, dayIndex) =>
+              <div key={`${dayIndex}`} className={classes.row}>
                 {this.getDayGrid(dayIndex, dayValues)}
-              </Grid>)
+              </div>)
           }
-        </Grid>
+        </div>
 
 
       </div>
