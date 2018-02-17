b5bb531977e35f96d02f06480c8e3eba25e18851
drorlevy <drorlevy@gmail.com>
2018-02-17 23:17:31 +0200

    responsive drawer

--- client/src/modules/components/AppFrame.js
+++ client/src/modules/components/AppFrame.js
@@ -20,6 +20,9 @@ import Divider from 'material-ui/Divider';
 import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import ScheduleIcon from 'material-ui-icons/Schedule';
 import PolicyIcon from 'material-ui-icons/LibraryBooks';
+import IconButton from 'material-ui/IconButton';
+import Hidden from 'material-ui/Hidden';
+import MenuIcon from 'material-ui-icons/Menu';
 
 // Lodash
 import map from 'lodash/map';
@@ -41,35 +44,89 @@ const links = [
   },
 ]
 
+// const styles = theme => ({
+//   root: {
+//     width: '100%',
+//     height: '100%',
+//     zIndex: 1,
+//     overflow: 'hidden',
+//     display: 'flex',
+//   },
+//   appBar: {
+//     // position: 'absolute',
+//     width: `calc(100% - ${drawerWidth}px)`,
+//     marginLeft: drawerWidth,
+//   },
+//   drawerPaper: {
+//     position: 'relative',
+//     height: '100%',
+//     width: drawerWidth,
+//   },
+//   drawerHeader: {
+//     ...theme.mixins.toolbar,
+//     display: 'flex',
+//     alignItems: 'center',
+//     width: drawerWidth,
+//     padding: `0 ${theme.spacing.unit * 3}px`
+//   },
+//   content: {
+//     overflow: 'auto',
+//     backgroundColor: theme.palette.background.default,
+//     width: '100%',
+//     padding: theme.spacing.unit,
+//     height: 'calc(100% - 56px)',
+//     marginTop: 56,
+//     [theme.breakpoints.up('sm')]: {
+//       height: 'calc(100% - 64px)',
+//       marginTop: 64,
+//     },
+//   },
+//   highlight: {
+//     backgroundColor: theme.palette.action.selected,
+//   }
+// });
+
 const styles = theme => ({
   root: {
     width: '100%',
     height: '100%',
     zIndex: 1,
     overflow: 'hidden',
+  },
+  appFrame: {
+    position: 'relative',
     display: 'flex',
+    width: '100%',
+    height: '100%',
   },
   appBar: {
-    // position: 'absolute',
-    width: `calc(100% - ${drawerWidth}px)`,
+    position: 'absolute',
     marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
   },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  drawerHeader: theme.mixins.toolbar,
   drawerPaper: {
-    position: 'relative',
-    height: '100%',
     width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      position: 'relative',
+      height: '100%',
+    },
   },
-  drawerHeader: {
-    ...theme.mixins.toolbar,
-    display: 'flex',
-    alignItems: 'center',
-    padding: `0 ${theme.spacing.unit * 3}px`
+  drawerDocked: {
+    height: '100%',
   },
   content: {
-    overflow: 'auto',
     backgroundColor: theme.palette.background.default,
     width: '100%',
     padding: theme.spacing.unit,
+    overflow: 'auto',
     height: 'calc(100% - 56px)',
     marginTop: 56,
     [theme.breakpoints.up('sm')]: {
@@ -77,16 +134,14 @@ const styles = theme => ({
       marginTop: 64,
     },
   },
-  highlight: {
-    backgroundColor: theme.palette.action.selected,
-  }
 });
 
 class AppFrame extends React.Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      title: ""
+      title: "",
+      mobileOpen: false
     }
   }
 
@@ -98,7 +153,7 @@ class AppFrame extends React.Component {
         title: currentLink.primary
       })
     }
-    
+
   }
 
   handleClickLink = link => event => {
@@ -109,24 +164,49 @@ class AppFrame extends React.Component {
     })
   }
 
+  handleDrawerToggle = () => {
+    this.setState((prevState, props) => ({ mobileOpen: !prevState.mobileOpen }));
+  };
+
   render() {
     const { classes, history, children } = this.props;
-    const { title } = this.state;
+    const { title, mobileOpen } = this.state;
+
+    // const drawer = (
+    //   <Drawer
+    //     variant="temporary"
+    //     open={drawerOpen}
+    //     onClose={this.handleCloseDrawer}
+    //     classes={{
+    //       paper: classes.drawerPaper,
+    //     }}
+    //     anchor="left"
+    //   >
+    //     <div className={classes.drawerHeader}>
+    //       <Typography variant="headline" color="secondary">
+    //         Zorya logo
+    //       </Typography>
+    //     </div>
+    //     <Divider />
+
+    //     <List dense disablePadding>
+    //       {
+    //         map(links, (link, index) =>
+    //           <ListItem key={index} className={classNames({ [classes.highlight]: startsWith(history.location.pathname, link.path) })} button onClick={this.handleClickLink(link)}>
+    //             <ListItemIcon>
+    //               {link.icon}
+    //             </ListItemIcon>
+    //             <ListItemText primary={link.primary} />
+    //           </ListItem>
+    //         )
+    //       }
+    //     </List>
+    //   </Drawer>
+    // );
 
     const drawer = (
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <div className={classes.drawerHeader}>
-          <Typography variant="headline" color="secondary">
-            Zorya logo
-          </Typography>
-        </div>
-
+      <div>
+        <div className={classes.drawerHeader} />
         <Divider />
         <List dense disablePadding>
           {
@@ -140,26 +220,66 @@ class AppFrame extends React.Component {
             )
           }
         </List>
-      </Drawer>
+      </div>
     );
 
     return (
       <div className={classes.root}>
+        <div className={classes.appFrame}>
 
-        <AppBar position="absolute" className={classes.appBar}>
-          <Toolbar>
-            <Typography variant="title" color="inherit" noWrap>
-              {title}
-            </Typography>
-          </Toolbar>
-        </AppBar>
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={this.handleDrawerToggle}
+                className={classes.navIconHide}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit" noWrap>
+                {title}
+              </Typography>
+            </Toolbar>
+          </AppBar>
 
-        {drawer}
+          <Hidden mdUp>
+            <Drawer
+              variant="temporary"
+              anchor="left"
+              open={this.state.mobileOpen}
+              classes={{
+                paper: classes.drawerPaper,
 
-        <div className={classes.content}>
-          {children}
+              }}
+              onClose={this.handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+
+          <Hidden smDown implementation="css">
+            <Drawer
+              variant="permanent"
+              anchor="left"
+              open
+              classes={{
+                paper: classes.drawerPaper,
+                docked: classes.drawerDocked
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+
+          <main className={classes.content}>
+            {children}
+          </main>
+
         </div>
-
       </div>
     );
   }
