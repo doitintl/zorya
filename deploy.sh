6e6cc7cdb388284eb0f330cdd014d75cdfdda354
drorlevy <drorlevy@gmail.com>
2018-02-21 11:41:53 +0200

    - fix bug when there are 2 projects that start with same id
    - deploy build client app

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECTID=`gcloud projects list | grep -i $1 |  awk '{print $1}'`
+PROJECTID=`gcloud projects list | grep -iw "$1" | awk '{print $1}'`
 if [ -z "$PROJECTID" ]; then
  echo Project $1 Not Found!
  exit
@@ -8,6 +8,8 @@ fi
 echo Project ID $PROJECTID
 gcloud config set project $PROJECTID
 
-
-
-gcloud app deploy -q app.yaml cron.yaml queue.yaml
+rm -rf ./build
+cd client
+yarn build
+cd ..
+gcloud app deploy app.yaml cron.yaml queue.yaml
