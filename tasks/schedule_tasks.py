58005f43eda31a2e7f89f189332adeae3071fb1b
fabricedupre <32126510+duprefm@users.noreply.github.com>
2019-04-08 16:01:39 +0200

    Add instancegroup schedule task
    Add instancegroup schedule task

--- tasks/schedule_tasks.py
+++ tasks/schedule_tasks.py
@@ -3,6 +3,7 @@ import logging
 
 from gcp.compute import Compute
 from gcp.sql import Sql
+from gcp.instances_group import Instancegroup
 
 
 def change_state(tagkey, tagvalue, action, project):
@@ -20,7 +21,9 @@ def change_state(tagkey, tagvalue, action, project):
 
     compute = Compute(project)
     sql = Sql(project)
+    instance_group = Instancegroup(project)
     logging.debug("change_state %s action %s", project, action)
     compute.change_status(action, tagkey, tagvalue)
     sql.change_status(action, tagkey, tagvalue)
+    instance_group.change_status(action, tagkey, tagvalue)
     return 'ok', 200
